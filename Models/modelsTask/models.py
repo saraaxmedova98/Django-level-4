0278e21e5e28bb992f1a8fb827f6dbd096cc4602
saraaxmedova98 <sariyataxmedova@gmail.com>
2020-06-06 19:23:06 +0400

    fifth task

--- Models/modelsTask/models.py
+++ Models/modelsTask/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
 
 class Products(models.Model):
@@ -25,3 +25,36 @@ class Products(models.Model):
         ordering = ['name']
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    description = models.TextField(max_length=500)
+
+class Car(models.Model):
+    name = models.CharField(max_length=125, unique=True)
+    production_date = models.DateField(auto_now_add=True)
+    is_new = models.BinaryField(default=False)
+    manufacturer = models.ForeignKey("modelsTask.Manufacturer", verbose_name=_("Manufacturer"), on_delete=models.CASCADE, null = True, blank = True)
+
+class Book(models.Model):
+    title = models.CharField(_("Title"), max_length=50)
+    description = models.TextField(_("Description"), blank=True)
+    price = models.DecimalField(_("Price"), max_digits=5, decimal_places=2)
+    page_count = models.IntegerField(_("Page count"))
+    cover_image = models.ImageField(_("Cover image"), upload_to="partials/images")
+    author = models.ForeignKey("modelsTask.Author", verbose_name=_("Author"), on_delete=models.CASCADE)
+
+class Author(models.Model):
+    fullname = models.CharField(_("Fullname"), max_length=50)
+    image = models.ImageField(_("Image"), upload_to="partials/images")
+    gender = models.BinaryField(_("Gender"))
+    date_of_birthday = models.DateField(_("Date of birthday"), auto_now_add=True)
+    nationality = models.CharField(_("Nationality"), max_length=50)
+    info = models.TextField(_("Info"), default="")
+
+class Category(models.Model):
+    title = models.TextField(_("Title"), default="")
+    books = models.ManyToManyField("modelsTask.Book", verbose_name=_("Book"))
+
+
+    
